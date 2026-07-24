1b8702b2a7bfcd6a410a8b33f527d0286c2d0bf9
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2026-07-24 09:57:33 +0700

    feat(anime): implement DRY helper and add new endpoints

--- src/endpoints/anime.js
+++ src/endpoints/anime.js
@@ -1,20 +1,59 @@
+import validateId from "../utils/commonValidator.js";
 
 export default function setupAnimeEndpoints(client) {
 
-    client.getAnimeById = async (id) => {
-        if (!id) throw new Error("ID Tidak Boleh Kosong");
-        const data = await client._request(`/anime/${id}/full`);
+    const fetchAnimeData = async (id, endpoint) => {
+        validateId(id);
+        const data = await client._request(`anime/${id}/${endpoint}`);
         return data;
+    }
+
+    client.getAnimeById = async (id) => {
+        return await fetchAnimeData(id, "full");
     }
 
     client.getAnimeCharacters = async (id) => {
-        if (!id) throw new Error("ID Tidak Boleh Kosong");
-        const data = await client._request(`anime/${id}/characters`);
-        return data;
+        return await fetchAnimeData(id, "characters");
     }
 
-    client.getAnimeStaff = async(id)=>{
-        
+    client.getAnimeStaff = async (id) => {
+        return await fetchAnimeData(id, "staff");
+    }
+
+    client.getAnimeEpisodes = async (id) => {
+        return await fetchAnimeData(id, "episodes");
+    }
+
+    client.getAnimeDetailEpisodes = async (id, episodeNumber) => {
+        return await fetchAnimeData(id, `episodes/${episodeNumber}`);
+    }
+
+    client.getAnimeNews = async (id) => {
+        return await fetchAnimeData(id, "news");
+    }
+
+    client.getAnimeForum = async (id) => {
+        return await fetchAnimeData(id, "forum");
+    }
+
+    client.getAnimeVideos = async (id) => {
+        return await fetchAnimeData(id, "videos");
+    }
+
+    client.getAnimePictures = async (id) => {
+        return await fetchAnimeData(id, "statistics");
+    }
+
+    client.getAnimeMoreInfo = async (id) => {
+        return await fetchAnimeData(id, "moreinfo");
+    }
+
+    client.getAnimeReviews = async (id) => {
+        return await fetchAnimeData(id, "reviews");
+    }
+
+    client.getAnimeRelations = async (id) => {
+        return await fetchAnimeData(id, "relations");
     }
 
 
