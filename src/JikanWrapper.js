5603080226df41d7726325ee7f7f5717879b95c4
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2026-07-27 13:02:17 +0700

    feat: add JSDoc for autocomplete

--- src/JikanWrapper.js
+++ src/JikanWrapper.js
@@ -1,11 +1,21 @@
 import setupAnimeEndpoints from "./endpoints/anime.js";
 
+/**
+ * JikanClient Class
+ * A wrapper for the Jikan API (v4).
+ */
 class JikanClient {
     constructor() {
         this.baseUrl = 'https://api.jikan.moe/v4';
         setupAnimeEndpoints(this);
     }
 
+    /**
+     * Internal method to make API requests.
+     * @private
+     * @param {string} endpoint - The API endpoint to request.
+     * @returns {Promise<Object>} The JSON response.
+     */
     async _request(endpoint) {
         const url = this.baseUrl + endpoint;
         try {
--- src/endpoints/anime.js
+++ src/endpoints/anime.js
@@ -1,5 +1,9 @@
 import validateId from "../utils/commonValidator.js";
 
+/**
+ * Setup anime endpoints for the JikanClient
+ * @param {import('../JikanWrapper.js').default} client 
+ */
 export default function setupAnimeEndpoints(client) {
 
     const fetchAnimeData = async (id, endpoint) => {
@@ -8,10 +12,18 @@ export default function setupAnimeEndpoints(client) {
         return data;
     }
 
+    /**
+     * Search for anime.
+     * @param {Object} queryParams - Search parameters.
+     * @param {string} [queryParams.q] - Search query (e.g., title).
+     * @param {number} [queryParams.page] - Page number.
+     * @param {number} [queryParams.limit] - Number of results per page.
+     * @returns {Promise<Object>} The anime search results.
+     */
     client.searchAnime = async (queryParams = {}) => {
         const queryString = new URLSearchParams(queryParams).toString();
 
-        const finalUrl = "/anime";
+        let finalUrl = "/anime";
         if (queryString) {
             finalUrl += `?${queryString}`;
         }
@@ -19,57 +31,122 @@ export default function setupAnimeEndpoints(client) {
         return await client._request(finalUrl);
     }
 
+    /**
+     * Get complete details of an anime by its ID.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} The anime details.
+     */
     client.getAnimeById = async (id) => {
         return await fetchAnimeData(id, "full");
     }
 
+    /**
+     * Get characters of an anime.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} The anime characters.
+     */
     client.getAnimeCharacters = async (id) => {
         return await fetchAnimeData(id, "characters");
     }
 
+    /**
+     * Get staff of an anime.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} The anime staff.
+     */
     client.getAnimeStaff = async (id) => {
         return await fetchAnimeData(id, "staff");
     }
 
+    /**
+     * Get episodes of an anime.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} The anime episodes.
+     */
     client.getAnimeEpisodes = async (id) => {
         return await fetchAnimeData(id, "episodes");
     }
 
+    /**
+     * Get a specific episode detail of an anime.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @param {number|string} episodeNumber - The episode number.
+     * @returns {Promise<Object>} The specific episode details.
+     */
     client.getAnimeDetailEpisodes = async (id, episodeNumber) => {
         return await fetchAnimeData(id, `episodes/${episodeNumber}`);
     }
 
+    /**
+     * Get news related to an anime.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} The anime news.
+     */
     client.getAnimeNews = async (id) => {
         return await fetchAnimeData(id, "news");
     }
 
+    /**
+     * Get forum topics related to an anime.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} The anime forum topics.
+     */
     client.getAnimeForum = async (id) => {
         return await fetchAnimeData(id, "forum");
     }
 
+    /**
+     * Get videos (promos, episodes) of an anime.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} The anime videos.
+     */
     client.getAnimeVideos = async (id) => {
         return await fetchAnimeData(id, "videos");
     }
 
+    /**
+     * Get pictures of an anime.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} The anime pictures.
+     */
     client.getAnimePictures = async (id) => {
         return await fetchAnimeData(id, "pictures");
     }
 
+    /**
+     * Get statistics of an anime.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} The anime statistics.
+     */
     client.getAnimeStatistics = async (id) => {
         return await fetchAnimeData(id, "statistics");
     }
 
+    /**
+     * Get more information about an anime.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} More information text.
+     */
     client.getAnimeMoreInfo = async (id) => {
         return await fetchAnimeData(id, "moreinfo");
     }
 
+    /**
+     * Get reviews of an anime.
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} The anime reviews.
+     */
     client.getAnimeReviews = async (id) => {
         return await fetchAnimeData(id, "reviews");
     }
 
+    /**
+     * Get relations of an anime (prequels, sequels, side stories, etc).
+     * @param {number|string} id - The Mal ID of the anime.
+     * @returns {Promise<Object>} The anime relations.
+     */
     client.getAnimeRelations = async (id) => {
         return await fetchAnimeData(id, "relations");
     }
-
 
 }
